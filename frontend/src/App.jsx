e798a4d61234f9bdeb5032fe6fb37f3dd863f9e2
unknown <kunalgoswami.2806@gmail.com>
2024-12-01 16:03:52 +0530

    myPortfolio first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Home from './Component/Home'
+import About from './Component/About'
+import Skills from './Component/Skills'
+import Education from './Component/Education'
+import Contact from './Component/Contact'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Education />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
